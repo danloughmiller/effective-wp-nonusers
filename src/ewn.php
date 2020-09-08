e794fd8af9fdb7872058007d79b5d3a85b30e501
Daniel Loughmiller <dan@effectwebagency.com>
2020-09-08 15:39:38 -0400

    -

--- src/ewn.php
+++ src/ewn.php
@@ -7,6 +7,7 @@ require_once('users.class.php');
 require_once('login.class.php');
 require_once('usermeta.class.php');
 require_once('roles.class.php');
+require_once('postroles.class.php');
 
 if (defined('EFFECTIVE_DEBUG'))
     require_once(realpath(__DIR__ . '/../tests/tests.php'));
